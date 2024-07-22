8ecd94c1dc3ba1ab8e7867de349f938046606825
noeltupas <manuel.tupas@gmail.com>
2024-07-22 16:03:28 -0400

    Added abstract class with base implementation.

---
+++ TestRepositoryCode001/TestRepositoryCode001/ITestRepoOutput.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRepositoryCode001;
+
+
+public interface ITestRepoOutput
+{
+	void Output(string str);
+}
+
+public class AbstractTestChildDependency(int times) : ITestRepoOutput
+{
+	public void Output(string str)
+	{
+		for (int i = 0; i < times; i++)
+		{
+			Console.WriteLine(str);
+		}
+	}
+}
+
+
+public class TestChildDependency1() : AbstractTestChildDependency(1) { }
+public class TestChildDependency2() : AbstractTestChildDependency(2) { }
+public class TestChildDependency3() : AbstractTestChildDependency(3) { }
+public class TestChildDependency4() : AbstractTestChildDependency(4) { }
+public class TestChildDependency5() : AbstractTestChildDependency(5) { }
